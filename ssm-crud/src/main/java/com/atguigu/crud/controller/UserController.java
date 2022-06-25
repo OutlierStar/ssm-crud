c42e655d90fc4ef92823a9f0c6fd211a58443f6f
xiemaowangdegithub <2545519283@qq.com>
2022-06-25 09:45:19 +0800

    2022.6.25.9.45

--- ssm-crud/src/main/java/com/atguigu/crud/controller/UserController.java
+++ ssm-crud/src/main/java/com/atguigu/crud/controller/UserController.java
@@ -1,35 +1,36 @@
 package com.atguigu.crud.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-
+import com.atguigu.crud.bean.Msg;
 import com.atguigu.crud.bean.User;
-import com.atguigu.crud.dao.UserMapper;
+import com.atguigu.crud.service.OrderService;
 import com.atguigu.crud.service.UserService;
 
 @Controller
+
 public class UserController {
-	
 	
 	@Autowired
 	private UserService userService;
 	
-	
-	
-	@RequestMapping("/emps")	
-	public void  getDepts(){
-//			List<User> list = userService.getAllUser();
-//		for(User t:list)
-//			System.out.println(t.getUserName());
+	public Msg login(User user) {
 		
+		User userRight = userService.check(user.getUserAccount(), user.getUserPassword());
 		
-		User t = userService.check("123", "123");
-			System.out.println(t.getUserName());
+		if(userRight != null) {//不为空，则登陆成功
+			
+			
+			return Msg.success().add("user", userRight);
+			
+		}else {//登陆失败
+			
+			
+			return Msg.fail().add("user", null);
+			
+		}
+		
 	}
 	
 	
--- ssm-crud/src/main/java/com/atguigu/crud/service/ShoppingCartService.java
+++ ssm-crud/src/main/java/com/atguigu/crud/service/ShoppingCartService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.atguigu.crud.bean.ShoppingCart;
+import com.atguigu.crud.bean.User;
 import com.atguigu.crud.dao.ShoppingCartMapper;
 
 @Service
