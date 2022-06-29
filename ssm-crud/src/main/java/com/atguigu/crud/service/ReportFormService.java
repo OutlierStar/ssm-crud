44365dc248a9181c3d992a6248665e32098b7d66
gojumpling <495096496@qq.com>
2022-06-29 11:37:39 +0800

    报表6-29 上午

--- ssm-crud/src/main/java/com/atguigu/crud/service/ReportFormService.java
+++ ssm-crud/src/main/java/com/atguigu/crud/service/ReportFormService.java
@@ -1,17 +1,20 @@
 package com.atguigu.crud.service;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.atguigu.crud.bean.Orders;
-import com.atguigu.crud.bean.OrdersExample;
 import com.atguigu.crud.dao.OrdersMapper;
 
 @Service
@@ -36,11 +39,33 @@ public class ReportFormService {
         //设置数据	,数值,第几条线,分类
     	
         for(Orders t :list)
-        	dataset.addValue( t.getOrderPrice() , "111" , t.getOrderTime() );        
- 
+        	dataset.addValue( t.getOrderPrice() , "营销额" , t.getOrderTime() );        
+
     	JFreeChart jfreeChart = ChartFactory.createLineChart("当天营销额折线图","时间","营销额",
     			dataset, PlotOrientation.VERTICAL, true, false,
      			false);
+    	
+    	
+
+    	
+        CategoryPlot plot = jfreeChart.getCategoryPlot(); 
+
+    	 LineAndShapeRenderer renderer  =  (LineAndShapeRenderer)plot.getRenderer();
+         DecimalFormat decimalformat1  =   new  DecimalFormat( " ##.## " ); // 数据点显示数据值的格式
+         renderer.setItemLabelGenerator( new  StandardCategoryItemLabelGenerator( " {2} " , decimalformat1));
+                                            // 上面这句是设置数据项标签的生成器
+         renderer.setItemLabelsVisible( true ); // 设置项标签显示
+         renderer.setBaseItemLabelsVisible( true ); // 基本项标签显示
+        
+//     	int width = 640; /* Width of the image */
+//       int height = 480; /* Height of the image */ 
+//       File pieChart = new File( "src//main//webapp//img//2.png" ); 
+//       try {
+// 			ChartUtilities.saveChartAsPNG( pieChart , jfreeChart , width , height );
+// 		} catch (IOException e) {
+// 			// TODO Auto-generated catch block
+// 			e.printStackTrace();
+// 		}
 	  	ChartFrame chartFrame = new ChartFrame("标题", jfreeChart);
 	  	chartFrame.pack();
 	  	chartFrame.setVisible(true);
@@ -59,11 +84,34 @@ public class ReportFormService {
         //设置数据	,数值,第几条线,分类
     	
         for(Orders t :list)
-        	dataset.addValue( t.getOrderPrice() , "111" , t.getOrderTime() );        
+        	dataset.addValue( t.getOrderPrice() , "订单量" , t.getOrderTime() );        
  
     	JFreeChart jfreeChart = ChartFactory.createLineChart("当天订单量折线图","时间","订单量",
     			dataset, PlotOrientation.VERTICAL, true, false,
      			false);
+    	
+
+    	CategoryPlot plot = jfreeChart.getCategoryPlot(); 
+
+   	 LineAndShapeRenderer renderer  =  (LineAndShapeRenderer)plot.getRenderer();
+        DecimalFormat decimalformat1  =   new  DecimalFormat( " ##.## " ); // 数据点显示数据值的格式
+        renderer.setItemLabelGenerator( new  StandardCategoryItemLabelGenerator( " {2} " , decimalformat1));
+                                           // 上面这句是设置数据项标签的生成器
+        renderer.setItemLabelsVisible( true ); // 设置项标签显示
+        renderer.setBaseItemLabelsVisible( true ); // 基本项标签显示
+       
+    	
+        
+//      int width = 640; /* Width of the image */
+//      int height = 480; /* Height of the image */ 
+//      File pieChart = new File( "src//main//webapp//img//2.png" ); 
+//      try {
+//			ChartUtilities.saveChartAsPNG( pieChart , jfreeChart , width , height );
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+    	
 	  	ChartFrame chartFrame = new ChartFrame("标题", jfreeChart);
 	  	chartFrame.pack();
 	  	chartFrame.setVisible(true);
